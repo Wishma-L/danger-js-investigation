637ec20753b6e687d31e5793b500adcd90bcff67
Wishma Herath <wishma.herath@clouda.io>
2022-10-31 12:17:45 +0530

    Add custom-page2

---
+++ src/pages/custom-page2.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+type CustomPage1Props = {}
+
+const CustomPage1 = ({}: CustomPage1Props) => {
+    return (
+      <>
+        <h1>React TS FC Component</h1>
+        <div>List</div>
+      </>
+    )
+  };
+
+export default CustomPage1
