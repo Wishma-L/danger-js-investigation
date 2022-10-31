e4856fdc647ab53ec6862cf824ce8c26b7e04803
Wishma Herath <wishma.herath@clouda.io>
2022-11-01 05:00:57 +0530

    Add page 3

---
+++ src/pages/custom-page3.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+type CustomPage1Props = {}
+
+const CustomPage1 = ({}: CustomPage1Props) => {
+    return (
+      <>
+        <h1>CustomPage 3 </h1>
+        <div>page list</div>
+      </>
+    )
+  };
+
+export default CustomPage1
